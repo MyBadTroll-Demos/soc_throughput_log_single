b2ec414d6006f439c6b7d5656a8653f463bd5fbe
Justin Reina <justinmreina@gmail.com>
2022-01-05 23:58:04 -0800

    (+)    final project layout cleanup in dir org

--- project/autogen/gatt_db.h
+++ project/autogen/gatt_db.h
@@ -15,17 +15,17 @@ extern const sli_bt_gattdb_t gattdb;
 #define gattdb_device_name                    11
 #define gattdb_manufacturer_name_string       16
 #define gattdb_system_id                      18
-#define gattdb_throughput_indications         21
-#define gattdb_throughput_notifications       25
-#define gattdb_transmission_on                29
-#define gattdb_throughput_result              33
-#define gattdb_connection_phy                 38
-#define gattdb_connection_interval            42
-#define gattdb_responder_latency              46
-#define gattdb_supervision_timeout            50
-#define gattdb_pdu_size                       54
-#define gattdb_mtu_size                       58
-#define gattdb_ota_control                    63
+#define gattdb_ota_control                    21
+#define gattdb_throughput_indications         24
+#define gattdb_throughput_notifications       28
+#define gattdb_transmission_on                32
+#define gattdb_throughput_result              36
+#define gattdb_connection_phy                 41
+#define gattdb_connection_interval            45
+#define gattdb_responder_latency              49
+#define gattdb_supervision_timeout            53
+#define gattdb_pdu_size                       57
+#define gattdb_mtu_size                       61
 
 
 #endif // __GATT_DB_H
